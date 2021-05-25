4d00c4186102f55a3d396fc435d6935a21fedc8c
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-05-25 23:46:08 +0400

    Swagger added

--- hrms/src/main/java/javacamp/hrms/HrmsApplication.java
+++ hrms/src/main/java/javacamp/hrms/HrmsApplication.java
@@ -2,6 +2,11 @@ package javacamp.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -10,4 +15,9 @@ public class HrmsApplication {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("javacamp.hrms")).build();
+	}
 }
